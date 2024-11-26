0b087259def36f9db46f6e28c79178741ba93bc3
Atif <iamatif96@gmail.com>
2024-11-26 18:24:18 +0800

    Initial commit

---
+++ src/main/java/com/atif/SpringSecurityWithJWT/SpringSecurityWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.atif.SpringSecurityWithJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithJwtApplication.class, args);
+	}
+
+}
